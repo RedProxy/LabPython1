2db9b73081ef7092f10ff2e849f98083cb98948d
RedProxy <alwinrfthh@gmail.com>
2021-11-07 08:55:42 +0700

    menambahkan file

---
+++ Lab2Lat1.py
@@ -0,0 +1,10 @@
+print("Program Menentukan Bilangan Terbesar")
+print("====================================")
+
+a = int(input("Masukan nilai a: "))
+b = int(input("Masukan nilai b: "))
+
+if a > b:
+    print ("A yang terbesar")
+elif b > a:
+    print ("B yang terbesar")
